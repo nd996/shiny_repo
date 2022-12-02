2b2e89e07fe491368e8ecce3c78ac8f4c012da72
Neil Douglas <neil.douglas@york.ac.uk>
2022-12-02 16:34:13 +0000

    tidy up

--- modules/shiny_server/manifests/shiny_apps.pp
+++ modules/shiny_server/manifests/shiny_apps.pp
@@ -1,9 +1,5 @@
 class shiny_server::shiny_apps {
-  # put list of R packages to install for shiny apps
-  # syntx is: 
-  #       profile::install_package {
-  #          '<package_Name>': dependencies => true,  # optional, default is FALSE 
-  #       }
+
   shiny_server::install_shiny_app { 
     'test': repo => 'https://github.com/yhejazi/tutorials.git',
   }
